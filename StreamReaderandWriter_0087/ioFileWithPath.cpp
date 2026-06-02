3f858bf02777458750d587b3867e24d324fb71bf
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-06-02 16:56:05 +0700

    deklarasi variabel baris dan NamaFile, me, mbuat input untuk menangkap nama file, menunjuk ke sebuah nama file dengan ekstentsi txt

--- StreamReaderandWriter_0087/ioFileWithPath.cpp
+++ StreamReaderandWriter_0087/ioFileWithPath.cpp
@@ -2,3 +2,19 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
+int main()
+{
+    //deklarasi variabel baris dan NamaFile
+    string baris, NamaFile;
+
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+
+     //membuat file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file dengan ekstentsi txt
+    outfile.open(NamaFile + ".txt", ios::out);
+
+     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
