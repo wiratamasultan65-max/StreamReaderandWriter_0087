0f956a14ad808758435ea6b1602bd2b2ddc579bc
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-06-02 16:46:44 +0700

    kondisi jika file ada dan kondisi jika file ada

--- StreamReaderandWriter_0087/StreamReaderandWriter.cpp
+++ StreamReaderandWriter_0087/StreamReaderandWriter.cpp
@@ -35,3 +35,20 @@ int main()
     ifstream infile;
     //menunjuk ke sebuah file
     infile.open("contohfile.txt");
+
+     cout << endl << ">=Membuka dan membaca file " << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
+
+     {
+        //perulangan untuk memunculkan setip baris
+        while (getline(infile, baris)) {
+            cout << baris << endl;
+        }
+        //menutup file jika telah selesai
+        infile.close();
+    }
+    //kondisi jika file ada
+    else cout << "Unable to open file";
+    return 0;
+}
