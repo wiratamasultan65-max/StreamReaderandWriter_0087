cbe4643848951df7bef953cebbe28dad661eaaa7
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-06-02 16:45:29 +0700

    membuka file dalam mode membaca

--- StreamReaderandWriter_0087/StreamReaderandWriter.cpp
+++ StreamReaderandWriter_0087/StreamReaderandWriter.cpp
@@ -30,3 +30,8 @@ int main()
 
     //menutup file setelah penulisan selesai
     outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
