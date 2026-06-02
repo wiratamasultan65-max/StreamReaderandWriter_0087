fe5775e27f636c39b3a9495eec55f2b399147912
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-06-02 16:53:55 +0700

    deklarasi array data dengan panjang index 3

--- StreamReaderandWriter_0087/exceptionStandart.cpp
+++ StreamReaderandWriter_0087/exceptionStandart.cpp
@@ -17,3 +17,9 @@ int main()
      //kondisi jika terjadi error didalam try
     catch (exception& e)
     {
+          cout << e.what() << endl;
+    }
+    cout << "Baris program terakhir" << endl; //penanda 2
+    return 0;
+
+}
